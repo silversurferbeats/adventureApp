1a3c9e97e170815ea4a214d29339e0a10579adae
silversurferbeats <gabrielsuarez89@gmail.com>
2021-05-11 11:48:13 -0300

    Updates

---
+++ precache-manifest.ac509bac2a6e9517d4ab867bbb287fd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f817868bcd8eb6739c20f8d6efbdfd59",
+    "url": "/adventureApp/index.html"
+  },
+  {
+    "revision": "8c2c6b664a671c46f649",
+    "url": "/adventureApp/static/css/main.474ddbc6.chunk.css"
+  },
+  {
+    "revision": "36128ff7f373e5beff7a",
+    "url": "/adventureApp/static/js/2.1442e098.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/adventureApp/static/js/2.1442e098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c2c6b664a671c46f649",
+    "url": "/adventureApp/static/js/main.449d54e2.chunk.js"
+  },
+  {
+    "revision": "ac7b42d20b85d07d0cf9",
+    "url": "/adventureApp/static/js/runtime-main.ea12f415.js"
+  }
+]);
